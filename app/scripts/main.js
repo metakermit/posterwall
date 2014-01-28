82445e882f5857f53a53def87dfe2309bfa7ade5
Dražen Lučanin <kermit666@gmail.com>
2014-01-28 18:51:59 +0100

    webapp masonry helloworld

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
 console.log('\'Allo \'Allo!');
+
+var container = document.querySelector('#container');
+var msnry = new Masonry( container, {
+    // options
+    columnWidth: 200,
+    itemSelector: '.item'
+});
