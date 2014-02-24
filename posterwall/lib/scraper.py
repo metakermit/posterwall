0f16c45b1fa01644b9357cb89bbf24ad7501ff62
Dražen Lučanin <kermit666@gmail.com>
2014-02-24 23:07:06 +0100

    testing parallel download methods

--- posterwall/lib/scraper.py
+++ posterwall/lib/scraper.py
@@ -8,40 +8,16 @@ import sys
 
 import requests
 from requests_futures.sessions import FuturesSession
+from concurrent.futures import as_completed
 from bs4 import BeautifulSoup
 from PIL import ImageFile
 
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 
 #TODO: check gzip
-def _initialize_request_old(url, referer):
-
-    if not url.startswith(("http://", "https://")):
-        return
-
-    req = urllib.request.Request(url)
-    req.add_header('Accept-Encoding', 'gzip')
-    #if g.useragent:
-    #    req.add_header('User-Agent', g.useragent)
-    if referer:
-        req.add_header('Referer', referer)
-    return req
 
 def _initialize_request(url, referer):
     pass
-
-def _fetch_url_old(url, referer=None):
-    request = _initialize_request(url, referer=referer)
-    if not request:
-        return None, None
-    response = urllib.request.urlopen(request)
-    response_data = response.read()
-    content_encoding = response.info().get("Content-Encoding")
-    if content_encoding and content_encoding.lower() in ["gzip", "x-gzip"]:
-        buf = io.BytesIO(response_data)
-        f = gzip.GzipFile(fileobj=buf)
-        response_data = f.read()
-    return response.headers.get("Content-Type"), response_data
 
 def _fetch_url(url, referer=None, session=None):
     if session:
@@ -53,31 +29,6 @@ def _fetch_url(url, referer=None, session=None):
 
 #TODO: cache (for multiple links to the same image) !!!
 # http://requests-cache.readthedocs.org/en/latest/user_guide.html#usage
-def _fetch_image_size_old(url, referer):
-    """Return the size of an image by URL downloading as little as possible."""
-
-    request = _initialize_request_old(url, referer)
-    if not request:
-        return None
-
-    parser = ImageFile.Parser()
-    response = None
-    try:
-        response = urllib.request.urlopen(request)
-
-        while True:
-            chunk = response.read(1024)
-            if not chunk:
-                break
-
-            parser.feed(chunk)
-            if parser.image:
-                return parser.image.size
-    except urllib.error.URLError:
-        return None
-    finally:
-        if response:
-            response.close()
 
 def _fetch_image_size(url, referer, session=None):
     """Return the size of an image by URL downloading as little as possible."""
@@ -108,7 +59,7 @@ def _fetch_image_size(url, referer, session=None):
 class Scraper(object):
     def __init__(self, url):
         self.url = url
-        self.session = FuturesSession(max_workers=20)
+        self.session = FuturesSession(max_workers=100)
 
     def _extract_image_urls(self, soup):
         for img in soup.findAll("img", src=True):
@@ -130,6 +81,7 @@ class Scraper(object):
         max_size = (0, 0)
         max_url = None
         sizes = []
+        logging.debug(image_urls)
         for image_url in image_urls:
             size = _fetch_image_size(image_url, referer=self.url,
                                      session=self.session)
@@ -173,10 +125,33 @@ def test_get_image_size():
 
 #test_get_image_size()
 
+def parallel_get_sizes(img_urls):
+    session = FuturesSession(max_workers=100)
+    adapter = requests.adapters.HTTPAdapter(pool_connections=100, pool_maxsize=100)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    future_to_url = {session.get(url): url for url in img_urls}
+    for future in as_completed(future_to_url):
+        img_url = future_to_url[future]
+        try:
+            data = future.result()
+            #TODO: read chunks, parse image
+        except Exception as exc:
+            print('{} generated an exception: {}'.format(img_url, exc))
+        else:
+            print('{} : {}'.format(img_url, data.status_code))
+    return {}
+
+def test_parallel_get_sizes():
+    img_urls = ['https://www.kset.org/media/uploads/program/28-03-2014_masinko/masinko_fb_thumb.jpg', 'https://www.kset.org/media/frontend/images/kset_logo.png', 'https://www.kset.org/media/uploads/program/_2014/2014-02-26_game_night_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-02-28_ksetcaffe_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-02-27_demonstrator_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-03-02_stiholoporter_fb_thumb.jpg', 'https://www.kset.org/media/images/nopicture.png', 'https://www.kset.org/media/uploads/program/_2014/2014-03-15_marcelo_fb_thumb.jpg', 'https://www.kset.org/media/frontend/images/headers/drum.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-03-22_atheistrap_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-03-01_pankrti_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-03-09_hp_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/07-03-2014_hladno_pivo/hladno_pivo_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-03-08_hp_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/07-03-2014_hladno_pivo/hladno_pivo_semibig.jpg']
+    img_sizes = parallel_get_sizes(img_urls)
+    print(img_sizes)
+
 def test_scraper():
     scraper = Scraper(link)
     thumbnail = scraper.scrape()
     print(thumbnail)
 
 if __name__=="__main__":
-    test_scraper()
+    #test_scraper()
+    test_parallel_get_sizes()
