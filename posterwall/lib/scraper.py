98b1cbe135b8f7e03955819596f0821c0250dc87
Dražen Lučanin <kermit666@gmail.com>
2014-02-25 22:02:19 +0100

    parallel requests with chunks

--- posterwall/lib/scraper.py
+++ posterwall/lib/scraper.py
@@ -15,19 +15,16 @@ from PIL import ImageFile
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 
 #TODO: check gzip
-
-def _initialize_request(url, referer):
-    pass
-
 def _fetch_url(url, referer=None, session=None):
     if session:
-        response = session.get(url).result()
+        response = session.get(url)
     else:
         response = requests.get(url)
     return response.headers['content-type'], response.text
 
 
 #TODO: cache (for multiple links to the same image) !!!
+# (the image download is now ~ to Chrome speed)
 # http://requests-cache.readthedocs.org/en/latest/user_guide.html#usage
 
 def _fetch_image_size(url, referer, session=None):
@@ -38,8 +35,8 @@ def _fetch_image_size(url, referer, session=None):
 
     # with closing(session.get(url, stream=True)) as response:
     try:
-        if session: # TODO: yield execution until result arrives
-            response = session.get(url, stream=True).result()
+        if session:
+            response = session.get(url, stream=True)
         else:
             response = requests.get(url, stream=True)
         for chunk in response.iter_content(chunk_size=32):
@@ -54,12 +51,38 @@ def _fetch_image_size(url, referer, session=None):
         if response:
             response.close()
 
+def parallel_get_sizes(img_urls):
+    session = FuturesSession(max_workers=100)
+    adapter = requests.adapters.HTTPAdapter(pool_connections=100, pool_maxsize=100)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    future_to_url = {session.get(url, stream=True): url for url in img_urls}
+    sizes = {}
+    for future in as_completed(future_to_url):
+        img_url = future_to_url[future]
+        try:
+            response = future.result()
+            parser = ImageFile.Parser()
+            for chunk in response.iter_content(chunk_size=32):
+                if not chunk:
+                    break
+                parser.feed(chunk)
+                if parser.image:
+                    sizes[img_url] = parser.image.size
+                    break
+        except requests.RequestsException:
+            sizes[img_url] = None
+        finally:
+            if response:
+                response.close()
+    return sizes
+
 #TODO: profile, something is too slow
 # -> ssl handshake on every request
 class Scraper(object):
     def __init__(self, url):
         self.url = url
-        self.session = FuturesSession(max_workers=100)
+        self.session = requests.Session()
 
     def _extract_image_urls(self, soup):
         for img in soup.findAll("img", src=True):
@@ -82,6 +105,8 @@ class Scraper(object):
         max_url = None
         sizes = []
         logging.debug(image_urls)
+        #image_sizes = parallel_get_sizes(img_urls)
+        #TODO: use parallel_get_sizes
         for image_url in image_urls:
             size = _fetch_image_size(image_url, referer=self.url,
                                      session=self.session)
@@ -103,44 +128,6 @@ class Scraper(object):
 #link = "https://www.kset.org/dogadaj/2014-02-14-plesnjak-kino-valentinovo/"
 link = "https://www.kset.org/dogadaj/2014-03-07-hladno-pivo/"
 #link = "http://www.mochvara.hr/program/info/lynx-lynx-music-predstavlja-140419"
-
-def get_image_urls_on_page(link):
-    session = requests.Session()
-    resp = session.get(link)
-    html = resp.text
-    soup = BeautifulSoup(html)
-    images = soup.find_all('img')
-    print(images)
-
-    img = images[0]
-    img_url = img.attrs['src']
-    print('opening: ' + img_url)
-    webbrowser.open(img_url)
-
-def test_get_image_size():
-    session = requests.Session()
-    img_url = "https://www.kset.org/media/uploads/program/_2014/2014-02-14_valentinovo_fb_thumb.jpg"
-    size = _fetch_image_size(img_url, link, session=session)
-    print(size)
-
-#test_get_image_size()
-
-def parallel_get_sizes(img_urls):
-    session = FuturesSession(max_workers=100)
-    adapter = requests.adapters.HTTPAdapter(pool_connections=100, pool_maxsize=100)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
-    future_to_url = {session.get(url): url for url in img_urls}
-    for future in as_completed(future_to_url):
-        img_url = future_to_url[future]
-        try:
-            data = future.result()
-            #TODO: read chunks, parse image
-        except Exception as exc:
-            print('{} generated an exception: {}'.format(img_url, exc))
-        else:
-            print('{} : {}'.format(img_url, data.status_code))
-    return {}
 
 def test_parallel_get_sizes():
     img_urls = ['https://www.kset.org/media/uploads/program/28-03-2014_masinko/masinko_fb_thumb.jpg', 'https://www.kset.org/media/frontend/images/kset_logo.png', 'https://www.kset.org/media/uploads/program/_2014/2014-02-26_game_night_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-02-28_ksetcaffe_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-02-27_demonstrator_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-03-02_stiholoporter_fb_thumb.jpg', 'https://www.kset.org/media/images/nopicture.png', 'https://www.kset.org/media/uploads/program/_2014/2014-03-15_marcelo_fb_thumb.jpg', 'https://www.kset.org/media/frontend/images/headers/drum.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-03-22_atheistrap_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-03-01_pankrti_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-03-09_hp_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/07-03-2014_hladno_pivo/hladno_pivo_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-03-08_hp_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/07-03-2014_hladno_pivo/hladno_pivo_semibig.jpg']
