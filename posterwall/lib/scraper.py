dc6d5b27eee60781a01189b8a117449435cb0f5b
Dražen Lučanin <kermit666@gmail.com>
2014-02-26 19:38:02 +0100

    use parallel downloading in Scraper

--- posterwall/lib/scraper.py
+++ posterwall/lib/scraper.py
@@ -16,46 +16,14 @@ logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 
 #TODO: check gzip
 def _fetch_url(url, referer=None, session=None):
-    if session:
-        response = session.get(url)
-    else:
-        response = requests.get(url)
+    response = session.get(url).result()
     return response.headers['content-type'], response.text
-
 
 #TODO: cache (for multiple links to the same image) !!!
 # (the image download is now ~ to Chrome speed)
 # http://requests-cache.readthedocs.org/en/latest/user_guide.html#usage
 
-def _fetch_image_size(url, referer, session=None):
-    """Return the size of an image by URL downloading as little as possible."""
-
-    parser = ImageFile.Parser()
-    response = None
-
-    # with closing(session.get(url, stream=True)) as response:
-    try:
-        if session:
-            response = session.get(url, stream=True)
-        else:
-            response = requests.get(url, stream=True)
-        for chunk in response.iter_content(chunk_size=32):
-            if not chunk:
-                break
-            parser.feed(chunk)
-            if parser.image:
-                return parser.image.size
-    except requests.RequestException:
-        return None
-    finally:
-        if response:
-            response.close()
-
-def parallel_get_sizes(img_urls):
-    session = FuturesSession(max_workers=100)
-    adapter = requests.adapters.HTTPAdapter(pool_connections=100, pool_maxsize=100)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
+def _parallel_get_sizes(img_urls, session):
     future_to_url = {session.get(url, stream=True): url for url in img_urls}
     sizes = {}
     for future in as_completed(future_to_url):
@@ -77,46 +45,37 @@ def parallel_get_sizes(img_urls):
                 response.close()
     return sizes
 
-#TODO: profile, something is too slow
-# -> ssl handshake on every request
 class Scraper(object):
     def __init__(self, url):
         self.url = url
-        self.session = requests.Session()
+        self.session = FuturesSession(max_workers=100)
+        adapter = requests.adapters.HTTPAdapter(pool_connections=100, pool_maxsize=100)
+        self.session.mount('http://', adapter)
+        self.session.mount('https://', adapter)
 
     def _extract_image_urls(self, soup):
         for img in soup.findAll("img", src=True):
             yield urljoin(self.url, img["src"])
 
     def _find_thumbnail_image(self):
-        # _fetch_content
         content_type, content = _fetch_url(self.url, session=self.session)
-        #resp = requests.get(self.url)
-        #html = resp.text
         soup = BeautifulSoup(content)
         image_urls = self._extract_image_urls(soup)
         image_urls = [u for u in image_urls] # turn to list
-        logging.debug('originally have {} images'.format(len(image_urls)))
         image_urls = list(set(image_urls)) # lose duplicates
-        logging.debug('fetching {} images'.format(len(image_urls)))
+        image_sizes = _parallel_get_sizes(image_urls, self.session)
+        logging.debug('got sizes for {} images'.format(len(image_sizes)))
         # find biggest
-        # TODO: use Reddit's procedure
+        # TODO: use Reddit's procedure !!!
         max_size = (0, 0)
         max_url = None
-        sizes = []
-        logging.debug(image_urls)
-        #image_sizes = parallel_get_sizes(img_urls)
-        #TODO: use parallel_get_sizes
         for image_url in image_urls:
-            size = _fetch_image_size(image_url, referer=self.url,
-                                     session=self.session)
+            size = image_sizes[image_url]
             if not size:
                 continue
-            sizes.append(size)
             if size > max_size:
                 max_size = size
                 max_url = image_url
-        logging.debug('got sizes for {} images'.format(len(sizes)))
         return max_url
 
 
@@ -129,16 +88,10 @@ class Scraper(object):
 link = "https://www.kset.org/dogadaj/2014-03-07-hladno-pivo/"
 #link = "http://www.mochvara.hr/program/info/lynx-lynx-music-predstavlja-140419"
 
-def test_parallel_get_sizes():
-    img_urls = ['https://www.kset.org/media/uploads/program/28-03-2014_masinko/masinko_fb_thumb.jpg', 'https://www.kset.org/media/frontend/images/kset_logo.png', 'https://www.kset.org/media/uploads/program/_2014/2014-02-26_game_night_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-02-28_ksetcaffe_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-02-27_demonstrator_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-03-02_stiholoporter_fb_thumb.jpg', 'https://www.kset.org/media/images/nopicture.png', 'https://www.kset.org/media/uploads/program/_2014/2014-03-15_marcelo_fb_thumb.jpg', 'https://www.kset.org/media/frontend/images/headers/drum.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-03-22_atheistrap_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-03-01_pankrti_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-03-09_hp_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/07-03-2014_hladno_pivo/hladno_pivo_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/_2014/2014-03-08_hp_fb_thumb.jpg', 'https://www.kset.org/media/uploads/program/07-03-2014_hladno_pivo/hladno_pivo_semibig.jpg']
-    img_sizes = parallel_get_sizes(img_urls)
-    print(img_sizes)
-
 def test_scraper():
     scraper = Scraper(link)
     thumbnail = scraper.scrape()
     print(thumbnail)
 
 if __name__=="__main__":
-    #test_scraper()
-    test_parallel_get_sizes()
+    test_scraper()
