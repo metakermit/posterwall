05da326fd8cf191c4b866c945d89a665c4a81e5d
Dražen Lučanin <kermit666@gmail.com>
2014-06-09 19:27:59 +0200

    fix rename link -> url migration

--- posterwall/apps/events/migrations/0002_auto__add_field_event_url__add_field_event_img_url.py
+++ posterwall/apps/events/migrations/0002_auto__add_field_event_url__add_field_event_img_url.py
@@ -8,20 +8,19 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding field 'Event.url'
-        db.add_column('events_event', 'url',
-                      self.gf('django.db.models.fields.URLField')(max_length=200, null=True),
-                      keep_default=False)
+        db.rename_column('events_event', 'link', 'url')
+        # Changing field 'Event.url'
+        db.alter_column('events_event', 'url', self.gf('django.db.models.fields.URLField')(max_length=200, null=True))
 
         # Adding field 'Event.img_url'
         db.add_column('events_event', 'img_url',
-                      self.gf('django.db.models.fields.URLField')(max_length=200, null=True),
+                      self.gf('django.db.models.fields.URLField')(null=True, max_length=200),
                       keep_default=False)
 
 
     def backwards(self, orm):
-        # Deleting field 'Event.url'
-        db.delete_column('events_event', 'url')
+        db.alter_column('events_event', 'url', self.gf('django.db.models.fields.URLField')(default='', max_length=200))
+        db.rename_column('events_event', 'url', 'link')
 
         # Deleting field 'Event.img_url'
         db.delete_column('events_event', 'img_url')
@@ -32,10 +31,9 @@ class Migration(SchemaMigration):
             'Meta': {'object_name': 'Event'},
             'description': ('django.db.models.fields.TextField', [], {}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'img_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True'}),
-            'link': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'img_url': ('django.db.models.fields.URLField', [], {'null': 'True', 'max_length': '200'}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
-            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True'})
+            'url': ('django.db.models.fields.URLField', [], {'null': 'True', 'max_length': '200'})
         }
     }
 
